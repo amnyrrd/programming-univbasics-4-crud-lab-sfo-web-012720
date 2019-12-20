424652bff327d03cadd55ed2648549ffd8813282
Aaron Minyard <amnyrrd@gmail.com>
2019-12-20 21:20:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,15 @@
 def create_an_empty_array
-  
+  arr = []
+  return arr
 end
 
 def create_an_array
-  
+  arr = [1,2,3,4]
+  return arr
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
